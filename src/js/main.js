d95a06285a6bcb468a1a1f0c143f8366c70f9b57
Abdul Halim <ahr.web.pro@gmail.com>
2023-03-19 17:13:10 +0600

    Add swiper carousel and design product list section

--- src/js/main.js
+++ src/js/main.js
@@ -1,1 +1,15 @@
+import Swiper from "swiper";
+import "swiper/css";
 import "../css/style.css";
+
+const swiper = new Swiper("#js--center-banner", {
+    speed: 400,
+    spaceBetween: 12,
+    autoplay: {
+        delay: 500,
+    },
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+});
